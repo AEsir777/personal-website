89f5c34194f53601fb1e9408551df642951f2689
AEsir777 <lkb2438834817@gmail.com>
2023-03-08 09:20:51 -0500

    migrate project to ejs and express

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+const frontendSkills = ['React', 'HTML', 'CSS', 'JavaScript'];
+const backendSkills = ['Python', 'SQL', 'Node.JS', 'Express', 'Django'];
+const MLSkills = ['Tensorflow', 'Sciket-learn', 'Jupyter', 'Anaconda'];
+const otherSkills = ['C++', 'Linux', 'Bash', 'git'];
+
+app.get('/', (req, res) => {
+    const parameters = {
+        frontendSkills: frontendSkills,
+        backendSkills: backendSkills,
+        MLSkills: MLSkills,
+        otherSkills: otherSkills
+    }
+    res.render('index', parameters);
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Server is running on port 3000.');
+});
