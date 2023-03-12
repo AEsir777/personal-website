262876216dcc4bccb86b556db3bfd0f64faeff02
AEsir777 <lkb2438834817@gmail.com>
2023-03-12 18:35:17 -0400

    finish skills part

--- app.js
+++ app.js
@@ -5,10 +5,82 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-const frontendSkills = ['React', 'HTML', 'CSS', 'JavaScript'];
-const backendSkills = ['Python', 'SQL', 'Node.JS', 'Express', 'Django'];
-const MLSkills = ["TensorFlow", "Scikit-learn", "Jupyter", "Anaconda"];
-const otherSkills = ["C++", "Linux", "Bash", "Git"];
+const frontendSkills = [
+  {
+    text: "React",
+    href: "images/React.png",
+  },
+  {
+    text: "HTML",
+    href: "images/HTML.png",
+  },
+  {
+    text: "CSS",
+    href: "images/CSS.png",
+  },
+  {
+    text: "JavaScript",
+    href: "images/JavaScript.png",
+  },
+];
+const backendSkills = [
+  {
+    text: "Python",
+    href: "images/Python.png",
+  },
+  {
+    text: "SQL",
+    href: "images/SQL.png",
+  },
+  {
+    text: "Node.JS",
+    href: "images/Node.JS.png",
+  },
+  {
+    text: "Express",
+    href: "images/Express.png",
+  },
+  {
+    text: "Django",
+    href: "images/Django.png",
+  },
+];
+const MLSkills = [
+  {
+    text: "TensorFlow",
+    href: "images/TensorFlow.png",
+  },
+  {
+    text: "Scikit-learn",
+    href: "images/Scikit-learn.png",
+  },
+  {
+    text: "Jupyter",
+    href: "images/Jupyter.png",
+  },
+  {
+    text: "Anaconda",
+    href: "images/Anaconda.png",
+  },
+];
+const otherSkills = [
+  {
+    text: "C++",
+    href: "images/C++.png",
+  },
+  {
+    text: "Linux",
+    href: "images/Linux.png",
+  },
+  {
+    text: "Bash",
+    href: "images/Bash.png",
+  },
+  {
+    text: "Git",
+    href: "images/Git.png",
+  },
+];
 
 app.get("/", (req, res) => {
   const parameters = {
