713fcf91b52acc46505c30625259ecd3eabbf29e
AEsir777 <lkb2438834817@gmail.com>
2023-03-12 13:39:40 -0400

    finish set up for the skills part

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ app.use(express.static('public'));
 
 const frontendSkills = ['React', 'HTML', 'CSS', 'JavaScript'];
 const backendSkills = ['Python', 'SQL', 'Node.JS', 'Express', 'Django'];
-const MLSkills = ['Tensorflow', 'Sciket-learn', 'Jupyter', 'Anaconda'];
-const otherSkills = ['C++', 'Linux', 'Bash', 'git'];
+const MLSkills = ["TensorFlow", "Scikit-learn", "Jupyter", "Anaconda"];
+const otherSkills = ["C++", "Linux", "Bash", "Git"];
 
-app.get('/', (req, res) => {
-    const parameters = {
-        frontendSkills: frontendSkills,
-        backendSkills: backendSkills,
-        MLSkills: MLSkills,
-        otherSkills: otherSkills
-    }
-    res.render('index', parameters);
+app.get("/", (req, res) => {
+  const parameters = {
+    frontendSkills: frontendSkills,
+    backendSkills: backendSkills,
+    MLSkills: MLSkills,
+    otherSkills: otherSkills,
+  };
+  res.render("index", parameters);
 });
 
 app.listen(process.env.PORT || 3000, function(){
