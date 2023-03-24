610b1d96de26e7a0fb6a4f205700e8b0e9fc993f
AEsir777 <lkb2438834817@gmail.com>
2023-03-23 23:54:58 -0400

    finish the titles

--- public/index.js
+++ public/index.js
@@ -1,15 +1,22 @@
-console.log('Javascript connected')
+console.log('Javascript connected');
+
+const arrowElement = '<span class="arrow"> &#10003;  <span>';
 
 function completePageRendering() {
   // add active class
   $(".carousel-item").first().addClass("active");
   $(".project-title-list li").first().addClass("active");
   $(".project-title-list li").first().attr("aria-current", "true");
+  $(".project-title-list li").first().prepend(arrowElement);
 
   // change according to slides carousel second indicator
   $(".carousel").on("slid.bs.carousel", function () {
+    // deal with non-active class
     $(".project-title-list li").removeClass("active");
     $(".project-title-list li").removeAttr("aria-current");
+    $(".project-title-list li").find(".arrow").remove();
+
+    // find active class
     let slide = $(".carousel-indicators button.active").data("bs-slide-to");
     $(".project-title-list")
       .find("[data-bs-slide-to='" + slide + "']")
@@ -17,8 +24,9 @@ function completePageRendering() {
     $(".project-title-list")
       .find("[data-bs-slide-to='" + slide + "']")
       .attr("aria-current", "true");
-
-    console.log(slide);
+      $(".project-title-list")
+      .find("[data-bs-slide-to='" + slide + "']")
+      .prepend(arrowElement);
   });
 };
 
