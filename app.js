89531a52cf2f9eeaf406e8c635581788838f5ee4
AEsir777 <lkb2438834817@gmail.com>
2023-04-09 16:31:20 -0400

    debug svg

--- app.js
+++ app.js
@@ -106,5 +106,5 @@ app.get("/projects/:projectId", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is running on port 3000.");
+  console.log("Server is running.");
 });
